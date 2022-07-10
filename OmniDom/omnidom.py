c46a630cf417e79cb91f30ee1f16c2a69d83467d
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-07-10 08:53:29 -0300

    gggg

--- OmniDom/omnidom.py
+++ OmniDom/omnidom.py
@@ -8,7 +8,7 @@ class OmniDom:
         self._perguntas = []
 
 
-    def _cria_value(self,nome:str,default:Any):
+    def _value_ou_default(self,nome:str,default:Any):
         value = self._respostas.get(nome)
         if not value:
             value = default
@@ -16,20 +16,26 @@ class OmniDom:
 
 
     def pergunta_boleana(self,nome:str,request:str,default:Any=None)->Pergunta:        
-        value = self._cria_value(nome,default)
-        pergunta = PerguntaBoleana(nome,request,value)
-        self._perguntas.append(pergunta)
-        return pergunta
-    
-    
-    def pergunta_texto(self,nome:str,request:str,default:Any=None)->Pergunta:        
-        value = self._cria_value(nome,default)
+        value = self._value_ou_default(nome,default)
         pergunta = PerguntaBoleana(nome,request,value)
         self._perguntas.append(pergunta)
         return pergunta
     
 
+    def pergunta_texto(self,nome:str,request:str,default:Any=None)->Pergunta:        
+        value = self.retorna_value_ou_default(nome,default)
+        pergunta = PerguntaBoleana(nome,request,value)
+        self._perguntas.append(pergunta)
+        return pergunta
     
+    
+    def lista_unica(self):
+        pass 
+
+    def lista_multipla(self):
+        pass 
+
+
     def _copile(self):
         return list(map(lambda p: p._render(),self._perguntas))
 
--- testes/testes_perguntas.py
+++ testes/testes_perguntas.py
@@ -1,6 +1,60 @@
 import unittest
 
+from OmniDom.perguntas import Pergunta, PerguntaBoleana
+
 
 class TestPerguntas(unittest.TestCase):
 
-    pass 
+    def test_criacao(self):
+        pergunta = Pergunta(
+            nome='teste',tipo=bool,
+            texto_da_pergunta='texto',
+            value=True)
+
+        self.assertEqual(pergunta._nome,'teste')
+        self.assertEqual(pergunta._tipo,'bool')
+        self.assertEqual(pergunta._texto_da_pergunta,'texto')
+        self.assertEqual(pergunta.value,True)
+
+
+    def test_render(self):
+        pergunta = Pergunta(
+            nome='teste',tipo=bool,
+            texto_da_pergunta='texto',
+            value=True)
+        esperado = {
+            'nome':'teste',
+            'tipo':'bool',
+            'pergunta':'texto',
+            'value': True
+        }
+        self.assertEqual(pergunta._render(),esperado)
+
+
+    def test_render_com_modificacao(self):
+        pergunta = Pergunta(
+            nome='teste',tipo=bool,
+            texto_da_pergunta='texto',
+            value=True)
+        pergunta.value = False
+        esperado = {
+            'nome':'teste',
+            'tipo':'bool',
+            'pergunta':'texto',
+            'value':False
+        }
+        self.assertEqual(pergunta._render(),esperado)
+
+    def test_pergunta_boleana(self):
+        pergunta = PerguntaBoleana('teste','texto',False)
+        self.assertEqual(pergunta._nome,'teste')
+        self.assertEqual(pergunta._tipo,'bool')
+        self.assertEqual(pergunta._texto_da_pergunta,'texto')
+        self.assertEqual(pergunta.value,False)
+
+    def test_pergunta_texto(self):
+        pergunta = Pergunta('teste','texto',False)
+        self.assertEqual(pergunta._nome,'teste')
+        self.assertEqual(pergunta._tipo,'bool')
+        self.assertEqual(pergunta._texto_da_pergunta,'texto')
+        self.assertEqual(pergunta.value,False)
