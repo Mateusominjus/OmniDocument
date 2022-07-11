d3bb7eb61d8ea2bfd2aaad9e5879100bde5b8ed1
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-07-11 10:28:27 -0300

    creatin setup tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='OmniDocument',
+   version='0.1.0',
+   author='Mateus',
+   author_email='mateusmoutinho01@gmail.com',
+   packages=['OmniDocument'],
+   url='https://github.com/Mateusominjus/OmniDocument.git',
+   description='Biblioteca para lidar com formulários da omnijus',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       "varname",
+   ]
+)
